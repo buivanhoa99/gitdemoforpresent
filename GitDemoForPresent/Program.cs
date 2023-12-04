cdc8af4f6b373ed98cd5996e98daf44531f5f92a
hoa.bui <hoa.bui@saigontechnology.com>
2023-12-04 21:25:03 +0700

    Them dong 123456 va copy comment

--- GitDemoForPresent/Program.cs
+++ GitDemoForPresent/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!123");
+Console.WriteLine("123456");
